bf5af2fadc1bc9d9f1662162383a203c6b28d566
borzX <jkornev2013@gmail.com>
2025-04-22 22:54:40 +1000

    all

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/helloworld', function () {
+    return view('welcome');
+});
+
+
+Route::get('/x', function () {
+    return view('employees');
+});
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts', ['adress'=>'Spb', 'post_code'=>'123000', 'email'=>'vasiliy@mail.ru', 'phone'=>'+7(123)999-88-77']);
+});
